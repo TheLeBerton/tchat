5f36fb7fe6bcaac87001e5978ef61eb3bc77a13d
leberton <theleberton@gmail.com>
2026-04-06 20:26:32 +0200

    feat: adapt main to new structure

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import typer
 
-import server
-import client
+from chat.server.server import ChatServer
+from chat.client.runner import run as client_run
 
 app: typer.Typer = typer.Typer()
 
 @app.command()
 def serv():
     try:
-        server.run()
+        ChatServer().start()
     except Exception as e:
         print( f"[ SERVER ERROR ]: { e }" )
 
 @app.command()
 def cli():
     try:
-        client.run()
+        client_run()
     except Exception as e:
         print( f"[ CLIENT ERROR ]: { e }" )
 
