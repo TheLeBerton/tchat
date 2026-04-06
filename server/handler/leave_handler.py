4c9c41062c67dc11d9571351d9f6de2c4bd176da
leberton <theleberton@gmail.com>
2026-04-06 17:54:31 +0200

    fix( server ): fix simple leaving bugs

--- server/handler/leave_handler.py
+++ server/handler/leave_handler.py
@@ -7,7 +7,10 @@ from message import Message, MessageType
 
 def handle( address: tuple ) -> None:
     with state.lock:
+        if address not in state.users:
+            return
         leave_broadcast = Message( type=MessageType.LEAVE, sender=state.users[ address ], content="left the chat", timestamp=datetime.now().strftime( "%H:%M" ) )
         del state.users[ address ]
+        del state.connections[ address ]
     logger.message( leave_broadcast )
     broadcast.cast( leave_broadcast.to_json(), address )
